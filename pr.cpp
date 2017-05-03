9acdecb7bcb4433bf0886f79ea8d11e1c29fa894
Artyom Grigoryev <u121044@edu.miet.ru>
2017-05-03 03:38:51 +0300

    End of project in master
    End of project in master

--- pr.cpp
+++ pr.cpp
@@ -1,27 +1,11 @@
 #include <iostream>
-#include <math.h>
 
 using namespace std;
 
 void main()
 {
-	cout << "Hello world!" << endl;
-	int sum;
-	int a = 12, b = 102;
-	sum = a + b;
-	cout << "Sum is " <<sum << end;
-
-	 
-	int diff;
-	int par1 = 29, par2 = 15;
-	diff = par1 - par2;
-	cout << "Difference between 29 and 15 is " << diff << endl;
-	
-	double expx = exp(3);
-	cout << "Exp in pow of 3 is " << expx << endl;
-
-
-	double var1 = 1001;
-	double var2 = var1 / 2;
-	cout << "Var2 is division 1001 to 2: " << var2 << endl;
+	char mass[] = {'a','b','c','d'};
+	for (int i = 0; i < sizeof(mass) / sizeof(mass[0]); i++)
+		cout << mass[i] << " ";
+	cout << "End of project!" << endl;
 }
