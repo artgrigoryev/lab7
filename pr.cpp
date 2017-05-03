b2f7605bd7b0e990ce2c6eda76e2e60fcf336e85
Artyom Grigoryev <u121044@edu.miet.ru>
2017-05-03 03:28:54 +0300

    Add mass of characters
    Add mass of characters in experimental1

--- pr.cpp
+++ pr.cpp
@@ -13,4 +13,8 @@ void main()
 	double var1 = 1001;
 	double var2 = var1 / 2;
 	cout << "Var2 is division 1001 to 2: " << var2 << endl;
+
+	char mass[] = {'a','b','c','d'};
+	for (int i = 0; i < sizeof(mass) / sizeof(mass[0]); i++)
+		cout << mass[i] << " ";
 }
