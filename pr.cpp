54beb1914480407d6e4e2ec3590bb239d9fd9f36
Artyom Grigoryev <u121044@edu.miet.ru>
2017-05-03 03:26:57 +0300

    Add division
    Add division in experimental1

--- pr.cpp
+++ pr.cpp
@@ -9,4 +9,8 @@ void main()
 	int a = 12, b = 102;
 	sum = a + b;
 	cout << "Sum is " <<sum << end;
+
+	double var1 = 1001;
+	double var2 = var1 / 2;
+	cout << "Var2 is division 1001 to 2: " << var2 << endl;
 }
